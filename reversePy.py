a94ed6e535001b6b4ed662713998a1fddddd832d
Dustin Chang <dustin_1022@msn.com>
2014-06-30 14:52:31 -0700

    Py reverse

---
+++ reversePy.py
@@ -0,0 +1,8 @@
+s = 'Hey Nexus'
+print s # prints s
+x = s[::-1] #reverses s
+print x #prints s reversed
+y = ''.join(reversed(s)) #reverses s
+print y #prints s reversed
+print 'hey yaaa'[::-1] #prints hey yaaa reversed
+print 'Nexus'[::-2]
